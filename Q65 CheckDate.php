4e044591bc4120010b7e0427a2d1c78d66b47be5
TanyaAggarwall <aggarwaltanya082@gmail.com>
2025-04-15 22:02:27 +0530

    Update Q65 CheckDate.php

--- Q65 CheckDate.php
+++ Q65 CheckDate.php
@@ -3,5 +3,5 @@
     var_dump(checkdate(12,31,-400));
     var_dump(checkdate(2,29,2003));
     var_dump(checkdate(2,29,2004));
-echo("<br>This program is written by Vardaan Valecha");
+echo("<br>This program is written by Tanya Aggarwal 0221BCA084");
 ?>
