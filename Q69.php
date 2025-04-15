2b6ed1c2c3c856ce6f14ac54be6f7bc30518058d
TanyaAggarwall <aggarwaltanya082@gmail.com>
2025-04-15 22:03:15 +0530

    Update Q69.php

--- Q69.php
+++ Q69.php
@@ -3,5 +3,5 @@ $bday=new DateTime('2001-08-05');
 $today=new DateTime();
 $age=$today->diff($bday);
 echo"your age is". $age->y."years".$age->m."months".$age->d."days";
-echo("<br>This program is written by Vardaan Valecha");
+echo("<br>This program is written by Tanya Aggarwal 0221BCA084");
 ?>
