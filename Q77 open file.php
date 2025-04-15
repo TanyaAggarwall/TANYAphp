64e6e66bafb746cf8cdb2df745f9bdab5e2ebff2
TanyaAggarwall <aggarwaltanya082@gmail.com>
2025-04-15 22:06:13 +0530

    Update Q77 open file.php

--- Q77 open file.php
+++ Q77 open file.php
@@ -10,5 +10,5 @@
         $text = fopen("welcome.txt","r");
         var_dump($text);
     }
-    echo("<br>This program is written by Vardaan Valecha");
+    echo("<br>This program is written by Tanya Aggarwal 0221BCA084");
 ?>
