234ba653e506a830a5a861a9ba537208ce4e4ea6
TanyaAggarwall <aggarwaltanya082@gmail.com>
2025-04-15 22:03:32 +0530

    Update Q70 unix timestamp.php

--- Q70 unix timestamp.php
+++ Q70 unix timestamp.php
@@ -4,5 +4,5 @@ $fd = date('Y-m-d H:i:s', $t);
 
 echo "Unix Timestamp: " . $t . "<br>";
 echo "Formatted Date: " . $fd . "<br>";
-echo("<br>This program is written by Vardaan Valecha");
+echo("<br>This program is written by Tanya Aggarwal 0221BCA084");
 ?>
